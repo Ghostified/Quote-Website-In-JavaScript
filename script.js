1f7ce35052932c4842baf8045df817697c1e5925
Ghostified <bransonallan@gmail.com>
2024-07-31 00:49:18 +0300

    fetched the content of the quote api arrray

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const api_url = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/quotes/random?tags=technology,famous-quotes";
+
+
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data)
+    //console.log(data)
+
+    //Since the data is an array
+    var quoteData = data[0];
+
+    quote.innerHTML = quoteData.content;
+    author.innerHTML = quoteData.author;
+
+    console.log(quoteData);
 }
 getquote(api_url);
